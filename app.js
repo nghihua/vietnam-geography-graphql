0b8468cc2c14e0e3e23cd4e821881dad5d0f8487
nghihua <huanhatgianghi@gmail.com>
2022-02-06 14:13:19 +0700

    change landing page to GraphQL Playground

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema/type-defs.js');
 const { resolvers } = require('./schema/resolvers.js');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	introspection: true,
-  	playground: true
+  	playground: true,
+  	plugins: [
+	    ApolloServerPluginLandingPageGraphQLPlayground()
+  	]
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({url}) => {
