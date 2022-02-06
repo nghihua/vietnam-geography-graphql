e5c0bac0e65816bbb7e73f4baddaf6b750e375f4
nghihua <huanhatgianghi@gmail.com>
2022-02-06 14:02:43 +0700

    enable introspection and playground

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema/type-defs.js');
 const { resolvers } = require('./schema/resolvers.js');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	introspection: true,
+  	playground: true
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({url}) => {
 	console.log(`
